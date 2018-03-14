34a9482312090c32d804964b39dcbee19b9a0943
casa <casatwy@msn.com>
2018-03-14 10:11:59 +0800

    Initial Commit

---
+++ CTJSBridge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CTJSBridge
+//
+//  Created by casa on 2018/3/14.
+//  Copyright © 2018年 casa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CTJSBridge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CTJSBridge
+//
+//  Created by casa on 2018/3/14.
+//  Copyright © 2018年 casa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
